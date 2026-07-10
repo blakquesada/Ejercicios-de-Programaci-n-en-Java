5b6d199233f248cdfee9301cc756059efb6b0ae2
blakquesada <anibalquesada15@gmail.com>
2026-07-10 15:56:18 -0500

    agregar nuevo ejercicio de nombres en java

---
+++ countName/src/countname/CountName.java
@@ -0,0 +1,33 @@
+
+package countname;
+
+import java.util.Scanner;
+
+
+public class CountName {
+
+   
+    public static void main(String[] args) {
+       String vector[] = new String [8]; 
+       
+       vector[0] = "alejandra";
+       vector[1] = "juan";
+        
+       vector[2] = "alejandra";
+       vector[3] = "juan";
+        
+       vector[4] = "alejandra";
+       vector[5] = "juan";
+        
+       vector[6] = "alejandra";
+       vector[7] = "juan";
+       
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("en la posicion  "+ i+ "esta el nombre"+ vector[i]);
+            
+        }
+       
+    
+}
+    
+}
